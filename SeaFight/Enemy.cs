63e5b511491476ffe9987510036d6aa5ca76fda4
Tanya Kolosenko <tanya.kolosenko.1705@gmail.com>
2023-07-03 13:17:39 +0300

    Add project files.

---
+++ SeaFight/Enemy.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace SeaFight
+{
+    internal class Enemy
+    {
+
+        public Button[,] myButtons = new Button[Form1.buttonHeight, Form1.buttonWidth];
+        public Button[,] enemyButtons = new Button[Form1.buttonHeight, Form1.buttonWidth];
+
+        public int[,] enemyMap = new int[Form1.buttonHeight, Form1.buttonWidth];
+        public int[,] myMap = new int[Form1.buttonHeight, Form1.buttonWidth];
+
+        
+
+        public Enemy(Button[,] myButtons, Button[,] enemyButtons, int[,] enemyMap, int[,] myMap)
+        {
+            this.myButtons = myButtons;
+            this.enemyButtons = enemyButtons;
+            this.enemyMap = enemyMap;
+            this.myMap = myMap;
+        }
+
+        public bool IsCellEmpty(int cell)
+        {
+
+            return (cell == 1) ? false : true;
+        }
+
+        public bool IsBuildShipExists(int y,int x, int shipLength)
+        {
+            for (int k = 0; k < shipLength; k++)
+            {
+                try
+                {
+                    if (IsCellEmpty(myMap[y, x + k]))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public void ShipsFilling (int y, int x, int shipLength)
+        {
+            for (int i = 0; i < shipLength; i++)
+            {
+                myMap[y,x + i] = 1;
+            }
+        }
+
+        public int[,] GenerateRandomShips()
+        {
+            int shipLength = 1;
+            int shipCount = 10;
+            int shipAmount = 4;
+
+            Random rand = new Random();
+
+            int xCoordinate;
+            int yCoordinate;
+
+
+            for (int i = shipCount; i >= 0; i--)
+            {
+                for (int s = shipAmount; s > 0; s--)
+                {
+                    xCoordinate = rand.Next(1, Form1.buttonHeight);
+                    yCoordinate = rand.Next(1, Form1.buttonWidth);
+
+                    Button button = myButtons[yCoordinate,xCoordinate];
+
+                    if (IsBuildShipExists(yCoordinate,xCoordinate, shipLength) == true) 
+                    {
+                        ShipsFilling(yCoordinate,xCoordinate, shipLength);
+                        shipCount--;
+                    }
+                    else 
+                    {
+                        s++;
+                        continue;
+                    }
+                }
+                shipAmount--;
+                shipLength++;
+            }
+            return myMap;
+
+        }
+        public void Atack()
+        {
+            Random rand = new Random();
+            while (true)
+            {
+                Button button = enemyButtons[rand.Next(1, Form1.buttonHeight), rand.Next(1, Form1.buttonWidth)];
+                if (IsCellEmpty(enemyMap[button.Location.Y, button.Location.X]) == true)
+                {
+                    button.BackColor = Color.Red;
+                    continue;
+                }
+                else
+                {
+                    button.BackColor = Color.Black;
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ SeaFight/Form1.cs
@@ -0,0 +1,156 @@
+namespace SeaFight
+{
+    public partial class Form1 : Form
+    {
+        public const int buttonHeight = 10;
+        public const int buttonWidth = 10;
+        public const int buttonSize = 35;
+        public const int distanseMaps = 450;
+
+        public static string latters = "ABCDEFGHI";
+        public static bool IsTheGameStarts = false;
+
+        public Button[,] myButtons = new Button[buttonHeight,buttonWidth];  
+        static Button[,] enemyButtons = new Button[buttonHeight, buttonWidth];  
+
+        static int [,] enemyMap = new int[buttonHeight, buttonWidth];  
+        static int [,] myMap = new int [buttonHeight, buttonWidth];
+
+        Enemy enemy;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            Realization();
+        }
+
+        public void Realization ()
+        {
+
+            enemy = new Enemy(enemyButtons, myButtons, myMap, enemyMap);
+
+            enemyMap = enemy.GenerateRandomShips();
+
+            Initialization();
+                      
+
+        }
+
+        public void Initialization ()
+        {
+            for (int i = 0; i < buttonHeight; i++)
+            {
+                //my realization
+                for (int k = 0; k < buttonWidth; k++)
+                {
+                    myMap[i,k] = 0;
+                    myButtons[i, k] = new Button();
+
+                    myButtons[i, k].Location = new Point(k * buttonSize, i * buttonSize);
+                    myButtons[i, k].Size = new Size(buttonSize, buttonSize);
+                    myButtons[i, k].BackColor = Color.AliceBlue;
+
+                    if (k == 0 || i == 0)
+                    {
+                        myButtons[i, k].BackColor = Color.Gray;
+
+                        if (k == 0 && i != 0)
+                        {
+                            myButtons[i, k].Text = i.ToString();
+                        }
+                        else if (i == 0 && k != 0)
+                        {
+                            myButtons[i, k].Text = latters[k-1].ToString();
+                        }
+                    }
+                    else
+                    {
+                        myButtons[i, k].Click += new EventHandler(PlaceShips);
+                    }
+                    this.Controls.Add(myButtons[i, k]);
+                }
+
+                //enemy realization
+                for (int k = 0; k < buttonWidth; k++)
+                {
+                    enemyButtons[i, k] = new Button();
+
+                    enemyButtons[i, k].Location = new Point(k * buttonSize + distanseMaps, i * buttonSize);
+                    enemyButtons[i, k].Size = new Size(buttonSize, buttonSize);
+
+                    if (k == 0 || i == 0)
+                    {
+                        enemyButtons[i, k].BackColor = Color.Gray;
+                        if (k == 0 && i != 0)
+                        {
+                        enemyButtons[i, k].Text = i.ToString();
+                        }
+                        else if (i == 0 && k != 0)
+                        {
+                        enemyButtons[i, k].Text = latters[k-1].ToString();
+                        }
+
+                    }
+                    else
+                    {
+                        enemyButtons[i, k].Click += new EventHandler(IsAtackSuccesfull);
+                        //enemy.Atack();
+                        
+                    }
+                    this.Controls.Add(enemyButtons[i, k]);
+                }
+
+                Button startButton = new Button();
+                startButton.Location = new Point(363, 380);
+                startButton.Text = "Start";
+                startButton.Click += new EventHandler(StartTheGame);
+
+                this.Controls.Add(startButton);
+            }
+        }
+        public void StartTheGame (object sender, EventArgs e)
+        {
+            Button currentButton = sender as Button;
+            if (IsTheGameStarts == false) 
+            {
+                IsTheGameStarts = true;
+            }
+        }
+
+        public void PlaceShips (object sender, EventArgs e)
+        {
+            if (!IsTheGameStarts)
+            {
+                Button currentButton = sender as Button;
+                if (myMap[currentButton.Location.Y / buttonSize, currentButton.Location.X / buttonSize] == 0)
+                {
+                    myMap[currentButton.Location.Y / buttonSize, currentButton.Location.X / buttonSize] = 1;
+                    currentButton.BackColor = Color.Green;
+                }
+                else
+                {
+                    myMap[currentButton.Location.Y / buttonSize, currentButton.Location.X / buttonSize] = 0;
+                    currentButton.BackColor = Color.AliceBlue;
+                }
+            }
+        }
+        public void IsAtackSuccesfull(object sender, EventArgs e)
+        {
+            if (IsTheGameStarts) 
+            {
+                Button currentButton = sender as Button;
+                if (enemyMap[currentButton.Location.Y / buttonSize, (currentButton.Location.X-distanseMaps) / buttonSize ] == 1)
+                {
+                    currentButton.BackColor = Color.Red;
+                }
+                else { currentButton.BackColor = Color.Blue; }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
